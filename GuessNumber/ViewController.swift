fde70aa46692a8bc94b4e7a76c8e070460501fe6
Unknown <t19960804@gmail.com>
2018-06-05 09:28:41 +0800

    0603
    結構錯誤

--- GuessNumber/ViewController.swift
+++ GuessNumber/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
-
+import SwifterSwift
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        if let urlStr = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000139-001".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: urlStr) {
+//            let task = URLSession.shared.dataTask(with: url) { (data, response , error) in
+//                let decoder = JSONDecoder()
+//                decoder.dateDecodingStrategy = .iso8601
+//                if let data = data, let object = try?
+//                    decoder.decode(Object.self, from: data)
+//                {
+//                    for records in
+//                    {
+//                        print("名稱:",records.title)
+//                    }
+//                }
+//                else {
+//                    print("error")
+//                }
+//            }
+//            task.resume()
+//        }
+        if let urlStr = "https://itunes.apple.com/search?term=薛之謙&media=music".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: urlStr) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response , error) in
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                if let data = data, let songResults = try?
+                    decoder.decode(SongResults.self, from: data)
+                {
+                    for song in songResults.results {
+                        print(song.trackName)
+                    }
+                } else {
+                    print("error")
+                }
+            }
+            task.resume()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+//    struct Object : Codable {
+//
+//        struct results : Codable {
+//            var records : eachdata
+//            struct eachdata : Codable{
+//                var title :String
+//                var address : String
+//            }
+//
+//        }
+//    }
+    
 }
-
+struct SongResults: Codable {
+    struct Song: Codable {
+        var artistName: String
+        var trackName: String
+        var collectionName: String?
+        var previewUrl: URL
+        var artworkUrl100: URL
+        var trackPrice: Double?
+        var releaseDate: Date
+        var isStreamable: Bool?
+    }
+    var resultCount: Int
+    var results: [Song]
+}
---
+++ Pods/Target Support Files/Pods-GuessNumberTests/Pods-GuessNumberTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuessNumberTests : NSObject
+@end
+@implementation PodsDummy_Pods_GuessNumberTests
+@end
